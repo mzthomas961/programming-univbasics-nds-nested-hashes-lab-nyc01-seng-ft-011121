fca0c90b5bf8ab2d0fe7a2a8c12bd4e08071a291
Matthew Thomas <mzthomas96@gmail.com>
2021-01-04 20:16:10 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,7 @@ def literal_aoh
     array = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
    {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
    {:name=>"Pat Monteseian", :occupation=>"Staff"},
+   
 
 def aoh_lookup(aoh, row, key)
   
